685c2c158b315c6b80bf499cf529ad2357a15244
EunJiHwang <e.hwang.business@gmail.com>
2024-08-09 09:52:32 +0900

    수정

---
+++ library/library/build/Desktop_Qt_6_5_3_MSVC2019_64bit-Debug/library_autogen/EWIEGA46WW/moc_mainwindow.cpp
@@ -0,0 +1,238 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../../mainwindow.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
+    "MainWindow",
+    "openFile",
+    "",
+    "saveFile",
+    "quit",
+    "addBook",
+    "deleteBook",
+    "searchBook",
+    "rentBook",
+    "returnBook",
+    "addUser",
+    "deleteUser",
+    "searchUser"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
+    uint offsetsAndSizes[26];
+    char stringdata0[11];
+    char stringdata1[9];
+    char stringdata2[1];
+    char stringdata3[9];
+    char stringdata4[5];
+    char stringdata5[8];
+    char stringdata6[11];
+    char stringdata7[11];
+    char stringdata8[9];
+    char stringdata9[11];
+    char stringdata10[8];
+    char stringdata11[11];
+    char stringdata12[11];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 8),  // "openFile"
+        QT_MOC_LITERAL(20, 0),  // ""
+        QT_MOC_LITERAL(21, 8),  // "saveFile"
+        QT_MOC_LITERAL(30, 4),  // "quit"
+        QT_MOC_LITERAL(35, 7),  // "addBook"
+        QT_MOC_LITERAL(43, 10),  // "deleteBook"
+        QT_MOC_LITERAL(54, 10),  // "searchBook"
+        QT_MOC_LITERAL(65, 8),  // "rentBook"
+        QT_MOC_LITERAL(74, 10),  // "returnBook"
+        QT_MOC_LITERAL(85, 7),  // "addUser"
+        QT_MOC_LITERAL(93, 10),  // "deleteUser"
+        QT_MOC_LITERAL(104, 10)   // "searchUser"
+    },
+    "MainWindow",
+    "openFile",
+    "",
+    "saveFile",
+    "quit",
+    "addBook",
+    "deleteBook",
+    "searchBook",
+    "rentBook",
+    "returnBook",
+    "addUser",
+    "deleteUser",
+    "searchUser"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      11,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   80,    2, 0x0a,    1 /* Public */,
+       3,    0,   81,    2, 0x0a,    2 /* Public */,
+       4,    0,   82,    2, 0x0a,    3 /* Public */,
+       5,    0,   83,    2, 0x0a,    4 /* Public */,
+       6,    0,   84,    2, 0x0a,    5 /* Public */,
+       7,    0,   85,    2, 0x0a,    6 /* Public */,
+       8,    0,   86,    2, 0x0a,    7 /* Public */,
+       9,    0,   87,    2, 0x0a,    8 /* Public */,
+      10,    0,   88,    2, 0x0a,    9 /* Public */,
+      11,    0,   89,    2, 0x0a,   10 /* Public */,
+      12,    0,   90,    2, 0x0a,   11 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSMainWindowENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'openFile'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'saveFile'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'quit'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'addBook'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'deleteBook'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'searchBook'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'rentBook'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'returnBook'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'addUser'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'deleteUser'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'searchUser'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->openFile(); break;
+        case 1: _t->saveFile(); break;
+        case 2: _t->quit(); break;
+        case 3: _t->addBook(); break;
+        case 4: _t->deleteBook(); break;
+        case 5: _t->searchBook(); break;
+        case 6: _t->rentBook(); break;
+        case 7: _t->returnBook(); break;
+        case 8: _t->addUser(); break;
+        case 9: _t->deleteUser(); break;
+        case 10: _t->searchUser(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 11)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 11;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 11)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 11;
+    }
+    return _id;
+}
+QT_WARNING_POP
---
+++ library/library/mainwindow.cpp
@@ -0,0 +1,130 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QTextEdit>
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
+#include <QLabel>
+#include <QTextEdit>
+#include <QStatusBar>
+#include <QMdiArea>
+#include <QMdiSubWindow>
+#include <QMessageBox>
+#include <QHash>
+#include <QFile>
+#include <QHBoxLayout>
+#include <QTableWidget>
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent) {
+
+    // mdiArea 생성
+    mdiArea = new QMdiArea(this);
+    setCentralWidget(mdiArea);
+    mdiArea->setViewMode(QMdiArea::TabbedView);     // 새창열기 모드 말고 탭으로 열기 모드 사용
+    mdiArea->addSubWindow(bookSubWin);
+    bookSubWin->setWindowTitle("Book");
+    mdiArea->addSubWindow(userSubWin);
+    userSubWin->setWindowTitle("User");
+    mdiArea->addSubWindow(rentSubWin);
+    rentSubWin->setWindowTitle("Rent/Return");
+
+    // bookSubWin
+    QWidget* bookWidget = new QWidget(); // mdiArea 에 바로 layout 추가 안된다
+    bookSubWin->setWidget(bookWidget);
+    QHBoxLayout* bookWin = new QHBoxLayout();
+    bookWin->setContentsMargins(0,0,0,0);
+
+    QTableWidget* bookList = new QTableWidget();
+    bookWin->addWidget(bookList);
+    QWidget* bookMenuWidget = new QWidget();
+
+    QVBoxLayout* bookMenuLayout = new QVBoxLayout();
+    bookMenuLayout
+    QPushButton* test1 = new QPushButton();
+    QPushButton* test2 = new QPushButton();
+    QPushButton* test3 = new QPushButton();
+    bookMenuLayout->addWidget(test1);
+    bookMenuLayout->addWidget(test2);
+    bookMenuLayout->addWidget(test3);
+    bookWin->addWidget(bookLabelMenu);
+
+    bookWidget->setLayout(bookWin);
+
+    // menubar 생성
+    QMenuBar* menuBar = new QMenuBar(this);
+    setMenuBar(menuBar);
+
+    // file menu 생성 후 menubar 에 추가
+    QMenu* fileMenu = menuBar->addMenu("&File");
+    // file menu 내에 action 생성
+    QAction* openAct = makeAction(":/images/open.png", "&open", "ctrl+O", "open existing file", this, SLOT(openFile()));
+    QAction* saveAct = makeAction(":/images/save.png", "&save", "ctrl+S", "save file", this, SLOT(saveFile()));
+    QAction* quitAct = makeAction(":/images/exit.png", "&quit", "ctrl+Q", "quit program", qApp, SLOT(quit()));
+    // file menu 내에 action 추가
+    fileMenu->addAction(openAct);
+    fileMenu->addAction(saveAct);
+    fileMenu->addAction(quitAct);
+
+    // book menu 내에 action 생성
+    QMenu* bookMenu = menuBar->addMenu("&Book");
+    QAction* addBook = makeAction(":/images/addBook.png", "&addBook", "ctrl+Z", "add book", this, SLOT(addBook()));
+    QAction* deleteBook = makeAction(":/images/deleteBook.png", "&deleteBook", "ctrl+Z", "delete book", this, SLOT(deleteBook()));
+    QAction* searchBook = makeAction(":/images/searchBook.png", "&searchBook", "ctrl+Z", "search book", this, SLOT(searchBook()));
+    QAction* rentBook = makeAction(":/images/rent.png", "rentBook", "ctrl+R", "rent book", this, SLOT(rentBook()));
+    QAction* returnBook = makeAction(":/images/return.png", "returnBook", "ctrl+shift+R", "return book", this, SLOT(returnBook()));
+    // book menu 내에 action 추가
+    bookMenu->addAction(addBook);
+    bookMenu->addAction(deleteBook);
+    bookMenu->addAction(searchBook);
+    bookMenu->addAction(rentBook);
+    bookMenu->addAction(returnBook);
+
+    // user menu 내에 action 생성
+    QMenu* userMenu = menuBar->addMenu("&User");
+    QAction* addUser = makeAction(":/images/addUser.png", "addUser", "shift+alt+A", "add user", this, SLOT(addUser()));
+    QAction* deleteUser = makeAction(":/images/deleteUser.png", "deleteUser", "shift+alt+D", "delete user", this, SLOT(deleteUser()));
+    QAction* searchUser = makeAction(":/images/searchUser.png", "searchUser", "shift+alt+s", "search user", this, SLOT(searchUser()));
+    // QAction* printOverdueUser = makeAction(":/images/print.png", "printOverdueUser", "shift+alt+P", "print overdue user", this, SLOT(printOverdueUser()));
+    // user menu 내에 action 추가
+    userMenu->addAction(addUser);
+    userMenu->addAction(deleteUser);
+    userMenu->addAction(searchUser);
+
+    // statusbar 추가
+    QStatusBar* statusbar = statusBar();
+    QLabel* todayDate = new QLabel("Today: 2024/08/08", statusbar);
+    QLabel* returnDate = new QLabel("Return: 2024/08/22", statusbar);
+    statusbar->addPermanentWidget(todayDate);
+    statusbar->addPermanentWidget(returnDate);
+
+}
+
+QAction* MainWindow::makeAction(QString icon, QString text, QString shortcut, QString tooltip, QObject* receiver, const char* slot){
+    QAction* tmp = new QAction(QIcon(icon), text, this);
+    tmp->setShortcut(shortcut);
+    tmp->setStatusTip(tooltip);
+    connect(tmp, SIGNAL(triggered(bool)), receiver, slot);
+    return tmp;
+}
+
+void MainWindow::openFile(){
+
+}
+
+void MainWindow::saveFile(){
+
+}
+
+void MainWindow::quit() {}
+
+void MainWindow::addBook() {}
+void MainWindow::deleteBook(){}
+void MainWindow::searchBook(){}
+void MainWindow::rentBook(){}
+void MainWindow::returnBook(){}
+
+void MainWindow::addUser(){}
+void MainWindow::deleteUser(){}
+void MainWindow::searchUser(){}
+
+MainWindow::~MainWindow() {}
